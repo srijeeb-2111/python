880816ab57263f864cd6cd292d41327ca3906701
Srijeeb <srijeeb@gmail.com>
2025-09-03 20:47:29 +0530

    new commit

---
+++ add2morenumbers.py
@@ -0,0 +1,7 @@
+# Ask the user for input
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+# Add and display the result
+sum = num1 + num2
+print("The sum is:", sum)
---
+++ add2numbers.py
@@ -0,0 +1,9 @@
+# Define two numbers
+num1 = 9874398
+num2 = 99999
+
+# Add the numbers
+sum = num1 + num2
+
+# Display the result
+print("The sum is:", sum)
